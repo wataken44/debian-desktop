9d00bb8b1dd289358b5b6ea513d7a46b5a832e78
wataken44 <wataken44@gmail.com>
2026-07-01 00:22:26 +0900

    initial commit

---
+++ bin/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BIN_DIR=$(cd $(dirname $0); pwd)/
+ROOT_DIR=$(cd $BIN_DIR; cd ..; pwd)/
+
+cd $ROOT_DIR
+
+if [ -x $BIN_DIR/env.sh ]; then
+    source $BIN_DIR/env.sh
+fi
+
+export LOGIN_USERNAME=$(ls --color=never -1 /home/ | head -n 1)
+ansible-playbook -i inventory.ini playbook.yml
+
+
+
+

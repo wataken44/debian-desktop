b265b2616c5ce728a7972c077b64aae7b007db4d
wataken44 <wataken44@gmail.com>
2026-07-01 00:39:38 +0900

    debug

--- bin/run.sh
+++ bin/run.sh
@@ -5,11 +5,12 @@ ROOT_DIR=$(cd $BIN_DIR; cd ..; pwd)/
 
 cd $ROOT_DIR
 
-if [ -x $BIN_DIR/env.sh ]; then
+if [ -f $BIN_DIR/env.sh ]; then
     source $BIN_DIR/env.sh
 fi
 
 export LOGIN_USERNAME=$(ls --color=never -1 /home/ | head -n 1)
+echo $PATH
 ansible-playbook -i inventory.ini playbook.yml
 
 
